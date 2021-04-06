8fba3091493babfdbd40d7ae720d6ca093b7e176
plinio <plinio.garcia@ifc.edu.br>
2021-04-06 14:32:01 -0300

    conectando na base de dados

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 //responsabilidade do index.js é apenas subir o servidor.
 
 const customExpress = require('./config/customExpress');
-const app = customExpress();
+const conexao = require('./infraestrutura/conexao');
 
-app.listen(3000, ()=> console.log("servidor rodando na porta 3000"));
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro);
+    }else{
+        console.log("conectado com sucesso");
+        const app = customExpress();
+
+        app.listen(3000, ()=> console.log("servidor rodando na porta 3000"));
+    }
+});
