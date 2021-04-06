7037fd65bf8e1077e5e89f25f4293ed51662360a
plinio <plinio.garcia@ifc.edu.br>
2021-04-06 14:00:46 -0300

    npm install body-parser - para poder ler os dados do body de uma requisição http

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = () => {
     //cria variável app com instância do servidor;
     const app = express();
+
+    //bibliotecas que vou usar
+    app.use(bodyParser.urlencoded({extended: true}));
 
     //configura as rotas do app
     consign()
