1fd47885634ae80ae8049cabc4482a9d069527fd
plinio <plinio.garcia@ifc.edu.br>
2021-04-06 15:45:03 -0300

    retorno com json e status http

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -7,8 +7,8 @@ module.exports = app => {
     app.post('/atendimentos',(req, res) => {
         const atendimento = req.body;
 
-        Atendimento.adiciona(atendimento);
-        res.send('POST atendimento.')
+        Atendimento.adiciona(atendimento, res);
+        
     })
 
 }
