e9ebe607c39015cce0aba47aee95787b179d917c
plinio <plinio.garcia@ifc.edu.br>
2021-04-06 14:07:39 -0300

    npm install mysql

--- config/customExpress.js
+++ config/customExpress.js
@@ -8,6 +8,7 @@ module.exports = () => {
 
     //bibliotecas que vou usar
     app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
 
     //configura as rotas do app
     consign()
