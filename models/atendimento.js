438d6b066dfeeefbfe6d1f33da95673ce88aed2b
plinio <plinio.garcia@ifc.edu.br>
2021-04-08 09:03:42 -0300

    consumindo dados do cliente de serviço externo

--- models/atendimento.js
+++ models/atendimento.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
@@ -54,13 +55,16 @@ class Atendimento{
     }
 
     buscaPorId(id, res){
-        const sql = `SELECT  * FROM Atendimentos WHERE id=${id}`;
-
-        conexao.query(sql, (erro, resultados)=>{
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+        
+        conexao.query(sql, async (erro, resultados)=>{
             const atendimento = resultados[0];
+            const cpf = atendimento.cliente;
             if(erro){
                 res.status(400).json(erro);
             }else{
+                const {data} = await axios.get(`http://localhost:8082/${cpf}`);
+                atendimento.cliente = data;
                 res.status(200).json(atendimento);
             }
         })
